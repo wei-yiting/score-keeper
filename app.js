fefeacffd95a04b323232c9f9b9fbad0d2750794
WYT <tina.yiting.wei@gmail.com>
2020-12-09 18:53:37 +0800

    refactoring app.js/ style select area/ add one more score option
    refactoring app.js by putting player1 and player2 properties into object. Creating a reusable addScore function and reset function to make the code less repeatable.
    
    use CSS to remove default style of select area and add some custom style to it
    
    add one more select option score "25"

--- app.js
+++ app.js
@@ -2,48 +2,50 @@ const toScore = document.querySelector('#toScore');
 let maxScore = 5;
 toScore.addEventListener('change', function(e){
     maxScore = parseInt(this.value);
+    reset();
 })
 
-const addP1 = document.querySelector('#addP1');
-const addP2 = document.querySelector('#addP2');
-const reset = document.querySelector('#reset');
-const p1score = document.querySelector('#p1score');
-const p2score = document.querySelector('#p2score');
+const player1 = {
+    score: 0,
+    addBtn: document.querySelector('#addP1'),
+    display: document.querySelector('#p1score')
+};
 
-let score1 = 0;
-let score2 = 0;
+const player2 = {
+    score: 0,
+    addBtn: document.querySelector('#addP2'),
+    display: document.querySelector('#p2score')
+};
 
-addP1.addEventListener('click', function(){
-    score1++;
-    p1score.innerText = score1;
-    if(score1 === maxScore){
-        addP1.disabled = true;
-        addP2.disabled = true;
-        p1score.classList.add('winner');
-        p2score.classList.add('loser');
+const resetBtn = document.querySelector('#reset');
+
+function addScore(player, opponent){
+    player.score += 1;
+    player.display.innerText = player.score;
+    if(player.score === maxScore){
+        player.addBtn.disabled = true;
+        opponent.addBtn.disabled = true;
+        player.display.classList.add('winner');
+        opponent.display.classList.add('loser');
     }
-})
+}
 
-addP2.addEventListener('click', function(){
-    score2++;
-    p2score.innerText = score2;
-    if(score2 === maxScore){
-        addP1.disabled = true;
-        addP2.disabled = true;
-        p1score.classList.add('loser');
-        p2score.classList.add('winner');
+player1.addBtn.addEventListener('click', function(){
+    addScore(player1, player2);
+});
+
+player2.addBtn.addEventListener('click', function(){
+    addScore(player2, player1);
+});
+
+resetBtn.addEventListener('click', reset);
+
+function reset(){
+    for(let player of [player1,player2]){
+        player.addBtn.disabled = false;
+        player.score = 0;
+        player.display.innerText = player.score;
+        player.display.classList.remove('winner', 'loser');
     }
-})
-
-reset.addEventListener('click', function(){
-    addP1.removeAttribute("disabled");
-    addP2.removeAttribute("disabled");
-    score1 = 0;
-    score2 = 0;
-    p1score.innerText = score1;
-    p2score.innerText = score2;
-    p1score.classList.remove('winner', 'loser');
-    p2score.classList.remove('winner', 'loser');
-})
-
+};
 
